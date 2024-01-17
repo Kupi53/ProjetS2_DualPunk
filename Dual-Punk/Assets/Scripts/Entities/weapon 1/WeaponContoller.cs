4820f058a1b0bbe1de62c7320d7b13f731b391cd
mael/splits <mael.minard@epita.fr>
2024-01-17 23:57:06 +0100

    Revert "Merge branch 'arnaud' into prototype"
    This reverts commit 1c3a4a24fd4c1b808a97d019702e686596d0d414, reversing
    changes made to 213e4bcfd9f440a58bdcaa672ad21396cfc5dc7f.

--- Dual-Punk/Assets/Scripts/Entities/weapon 1/WeaponContoller.cs
+++ Dual-Punk/Assets/Scripts/Entities/weapon 1/WeaponContoller.cs
@@ -1,48 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Reflection;
+using System.Threading;
 using UnityEngine;
 using System;
 
-public class M4Script : MonoBehaviour
+public class WeaponContoller : MonoBehaviour
 {
     public GameObject bullet;
-    public GameObject gunEnd;
     public GameObject pointer;
-    private GameObject? player;
-    public SpriteRenderer spriteRenderer;
+    public GameObject? player;
     private BulletScript bulletScript;
-    private bool inHand;
     private float timer;
     public float fireRate;
     public float weaponDistance;
+    public bool onGround;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
         timer = 0;
-        inHand = false;
+        onGround = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (inHand)
+        if (!onGround)
         {
             if (Input.GetButton("Drop"))
             {
                 player.GetComponent<Player>().HoldingWeapon = false;
-                inHand = false;
+                onGround = true;
             }
 
             Vector3 direction = (pointer.transform.position - player.transform.position).normalized;
             float angle = (float)(Math.Atan2(direction.y, direction.x) * (180 / Math.PI));
-            if (angle > 90 || angle < -90)
-                spriteRenderer.flipY = true;
-            else
-                spriteRenderer.flipY = false;
-
             transform.position = player.transform.position + direction * weaponDistance;
             transform.eulerAngles = new Vector3(0, 0, angle);
 
@@ -60,20 +54,22 @@ public class M4Script : MonoBehaviour
 
     void fireRound(Vector3 direction, float angle)
     {
-        GameObject newBullet = Instantiate(bullet, gunEnd.transform.position, transform.rotation);
+        GameObject newBullet = Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
         bulletScript = newBullet.GetComponent<BulletScript>();
         bulletScript.MoveDirection = direction;
         bulletScript.transform.eulerAngles = new Vector3(0, 0, angle);
     }
 
+    // lorsqu'on joueur collide avec le weapon
     void OnTriggerStay2D(Collider2D collisionInfo)
     {
+        Debug.Log("Collision");
         // si sur le sol
-        if (!inHand && Input.GetButton("Pickup"))
+        if (onGround && Input.GetButton("Pickup"))
         {
             player = collisionInfo.gameObject;
             player.GetComponent<Player>().HoldingWeapon = true;
-            inHand = true;
+            onGround = false;
         }
     }
 }
