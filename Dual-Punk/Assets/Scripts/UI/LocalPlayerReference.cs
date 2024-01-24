9c83692c6c8e62c1b262910b07074aaad14ea7ce
Arnaud <avepuit@gmail.com>
2024-01-24 09:45:22 +0100

    pas cool mael

--- Dual-Punk/Assets/Scripts/UI/LocalPlayerReference.cs
+++ Dual-Punk/Assets/Scripts/UI/LocalPlayerReference.cs
@@ -20,5 +20,4 @@ public class LocalPlayerReference : MonoBehaviour
             weaponScript = playerState.Weapon.GetComponent<WeaponScript>();
         }
     }
-
 }
--- Dual-Punk/Assets/Scripts/UI/TotalAmmo.cs
+++ Dual-Punk/Assets/Scripts/UI/TotalAmmo.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class TotalAmmo : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI Text;
+    
 
     void Update()
     {
