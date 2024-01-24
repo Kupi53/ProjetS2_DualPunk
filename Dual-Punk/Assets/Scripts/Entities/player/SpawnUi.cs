dad468d793980ccf92c7f1100f534c1731c02448
mael/splits <mael.minard@epita.fr>
2024-01-24 17:20:47 +0100

    jeu
    nouveau jeu pret a etre screenshot

--- Dual-Punk/Assets/Scripts/Entities/player/SpawnUi.cs
+++ Dual-Punk/Assets/Scripts/Entities/player/SpawnUi.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class SpawnUi : MonoBehaviour
+public class SpawnUi : NetworkBehaviour
 {
     [SerializeField] private GameObject UI;
     private GameObject LocalUI;
     void Start()
     {
-        GameObject LocalUI = Instantiate(UI);
-        LocalUI.name = gameObject.name + " UI";
-        LocalUI.GetComponent<LocalPlayerReference>().LOCALPLAYER = gameObject;
+        if (IsLocalPlayer)
+        {
+            GameObject LocalUI = Instantiate(UI);
+            LocalUI.name = gameObject.name + " UI";
+            LocalUI.GetComponent<LocalPlayerReference>().LOCALPLAYER = gameObject;
+        }
     }
 
 }
