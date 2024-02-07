11470a93418dce06852f951300a37ffed0ff694a
Arnaud <avepuit@gmail.com>
2024-02-07 10:55:31 +0100

    merge pour le fdp de mael :)

--- Dual-Punk/Assets/Scripts/Entities/Weapons/AutolockWeapon.cs
+++ Dual-Punk/Assets/Scripts/Entities/Weapons/AutolockWeapon.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class AutolockWeapon : WeaponScript
 {
+    private bool locked;
+
     void Start()
     {
         base.Start();
+        locked = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         base.Update();
+
+        if (inHand)
+        {
+            if (pointerScript.target != null)
+            {
+                if (Input.GetButtonDown("Switch"))
+                { locked = !locked;
+                    Debug.Log("locked");
+                }
+
+                if (locked)
+                    playerState.Pointer.transform.position = pointerScript.target.transform.position;
+            }
+            else
+                locked = false;
+        }
     }
 }
