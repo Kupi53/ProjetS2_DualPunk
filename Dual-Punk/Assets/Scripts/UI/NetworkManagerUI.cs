2b79d88d9920713f503469faa2b7573ce24387a8
mael/splits <mael.minard@epita.fr>
2024-02-12 14:43:42 +0100

    Revert "Merge branch 'mael' into prototype"
    This reverts commit 178bd59f916115f8b5258f35539410d4c9f5246e, reversing
    changes made to 29a5295ba8b610a80fdf6b6eb1b254aa730ab8c0.

--- Dual-Punk/Assets/Scripts/UI/NetworkManagerUI.cs
+++ Dual-Punk/Assets/Scripts/UI/NetworkManagerUI.cs
@@ -4,62 +4,51 @@ using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
 using UnityEngine.SceneManagement;
-using TMPro;
-using Unity.Netcode.Transports.UTP;
 
 public class NetworkManagerUI : NetworkBehaviour
 {
     [SerializeField] private Button hostButton;
-    [SerializeField] private Button connectButton;
-    [SerializeField] private TMP_InputField ipField;
+    [SerializeField] private Button clientButton;
+    [SerializeField] private GameObject playerPrefabA; //add prefab in inspector
+    [SerializeField] private GameObject playerPrefabB; //add prefab in inspector
+
 
     private void Awake(){
         hostButton.onClick.AddListener(() => {
-            StartCoroutine(LoadLobbyScene("host"));
+            StartCoroutine(LoadGameScene("host"));
         });
-        connectButton.onClick.AddListener(()=>{
-            StartCoroutine(LoadLobbyScene("client"));
+        clientButton.onClick.AddListener(() => {
+            StartCoroutine(LoadGameScene("client"));
         });
     }
+
 // g volé ce script dans la docu et modifié il permet de changer de scene de maniere clean
-    IEnumerator LoadLobbyScene(string type)
+    IEnumerator LoadGameScene(string type)
     {
-        // lobby scene
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Game", LoadSceneMode.Additive);
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Game"));
+        // spawn le joueur
         if (type=="host"){
-            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Lobby", LoadSceneMode.Additive);
-            // Wait until the asynchronous scene fully loads
-            while (!asyncLoad.isDone)
-            {
-                yield return null;
-            }
-            SceneManager.UnloadSceneAsync("Menu");
             NetworkManager.Singleton.StartHost();
+            /*GameObject player = Instantiate(playerPrefabA, new Vector3(0,0,0), Quaternion.identity).gameObject;
+            player.GetComponent<NetworkObject>().SpawnAsPlayerObject(NetworkManager.Singleton.LocalClientId);
+            Debug.Log("host de merde");*/
+            
         }
-        if (type == "client"){
-            NetworkManager.GetComponent<UnityTransport>().ConnectionData.Address = ipField.text;
-            if (NetworkManager.Singleton.StartClient()){
-                AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Lobby", LoadSceneMode.Additive);
-                // Wait until the asynchronous scene fully loads
-                while (!asyncLoad.isDone)
-                {
-                    yield return null;
-                }
-                SceneManager.UnloadSceneAsync("Menu");
-            }
+        else if (type=="client"){
+            NetworkManager.Singleton.StartClient();
+            /*Debug.Log("client de merde");
+            SpawnServerRpc(NetworkManager.Singleton.LocalClientId);*/
         }
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync("Menu");
     }
 
-    public void OnIpInputChanged(){
-        if (ipField.text == ""){
-            connectButton.interactable = false;
-            connectButton.GetComponentInChildren<TMP_Text>().color = new Color32(0, 185, 255, 100);
-        }
-        else{
-            connectButton.interactable = true;
-            connectButton.GetComponentInChildren<TMP_Text>().color = new Color32(0, 185, 255, 255);
-
-        }
-    }
     /*[ServerRpc(RequireOwnership = false)]
     public void SpawnServerRpc(ulong clientId){
         Debug.Log("client de merde11");
