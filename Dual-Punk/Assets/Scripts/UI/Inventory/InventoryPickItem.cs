b6798b69de63e2793f28144943a5e6b251dd9d8a
Nathan Revel <nathan.revel@gmail.com>
2024-03-24 22:49:20 +0100

    End of Inventory visual + DL Kasaya's inventory sprites

--- Dual-Punk/Assets/Scripts/UI/Inventory/InventoryPickItem.cs
+++ Dual-Punk/Assets/Scripts/UI/Inventory/InventoryPickItem.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InventoryPickItem : MonoBehaviour
 {
-    [SerializeField] GameObject[] Slots = new GameObject[30];
+    [SerializeField] GameObject[] weaponSlots = new GameObject[3];
+    [SerializeField] GameObject[] implantSlots = new GameObject [4];
+    [SerializeField] GameObject[] consummableSlots = new GameObject[3];
+    [SerializeField] GameObject[] inventorySlots = new GameObject[15];
     [SerializeField] GameObject inventoryItemPrefab;
 
     public void ItemPicked(GameObject pickedItem){
         GameObject emptySlot = null;
-        for (int i = 0 ; i < Slots.Length; i++){
+        string Tag = pickedItem.tag;
 
-            InventorySlots Slot = Slots[i].GetComponent<InventorySlots>();
-
-            if (Slot.heldItem == null){
-                emptySlot = Slots[i];
+        switch(Tag){
+            case "Item" :
+                emptySlot = FindEmptySlot(consummableSlots);
                 break;
-            }
+            case "Weapon" :
+                emptySlot = FindEmptySlot(weaponSlots);
+                break;
+            default :
+                emptySlot = FindEmptySlot(implantSlots);
+                break;
         }
+
+        if(emptySlot == null) emptySlot = FindEmptySlot(inventorySlots);
 
         if(emptySlot != null){
 
@@ -26,11 +36,26 @@ public class InventoryPickItem : MonoBehaviour
             //enregistre le prefab de displayed item car displayed item sera detruit juste en dessous
             newItem.GetComponent<InventoryItem>().displayedItem = pickedItem.GetComponent<PickableItem>().itemData;
 
-            newItem.transform.SetParent(emptySlot.transform.parent.parent.GetChild(1));
+            newItem.transform.SetParent(emptySlot.transform.parent.parent.GetChild(5));
             newItem.transform.localScale = emptySlot.transform.localScale;
             
             emptySlot.GetComponent<InventorySlots>().SetHeldItem(newItem);
             Destroy(pickedItem);
         }
+    }
+
+    //Use for each type of slot of the inventory to find if a slot is available.
+    public GameObject FindEmptySlot(GameObject[] slots){
+        GameObject res = null;
+        for (int i = 0 ; i < slots.Length; i++){
+
+            InventorySlots Slot = slots[i].GetComponent<InventorySlots>();
+
+            if (Slot.heldItem == null){
+                res = slots[i];
+                break;
+            }
+        }
+        return res;
     }
 }
