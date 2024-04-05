1f551ce053290667153aa5ac778f12e0099fa4ee
Nathan Revel <nathan.revel@gmail.com>
2024-04-05 11:08:16 +0200

    Fix inventaire, ne pas loot le rocketlauncher

--- Dual-Punk/Assets/Scripts/Entities/Player/Stats/PlayerPickItem.cs
+++ Dual-Punk/Assets/Scripts/Entities/Player/Stats/PlayerPickItem.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class PlayerPickItem : MonoBehaviour
 {
-    private InventoryPickItem inventoryManager;
+    private GameObject inventoryManager;
     private GameObject itemToPick;
     private bool _CanBePicked;
     
     // Start is called before the first frame update
     void Start()
     {
-        inventoryManager = GameObject.FindWithTag("Inventory").GetComponent<InventoryPickItem>();
+        inventoryManager = GameObject.FindWithTag("Inventory");
         inventoryManager.gameObject.SetActive(false);
     }
 
     void Update(){
         if(_CanBePicked && Input.GetButtonDown("Pickup")){
-            inventoryManager.ItemPicked(itemToPick);
+            inventoryManager.GetComponent<InventoryPickItem>().ItemPicked(itemToPick);
         }
     }
 
