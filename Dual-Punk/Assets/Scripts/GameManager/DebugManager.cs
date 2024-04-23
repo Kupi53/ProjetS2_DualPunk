be2c91238b780b2ab84c51feae6e51edd9a7dd00
Arnaud <avepuit@gmail.com>
2024-04-23 18:30:04 +0200

    nathan t as broke le prefab AR1

--- Dual-Punk/Assets/Scripts/GameManager/DebugManager.cs
+++ Dual-Punk/Assets/Scripts/GameManager/DebugManager.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class DebugManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _weaponToSpawn;
+    [SerializeField] private GameObject[] _weaponsToSpawn;
+    private int _index;
 
 
     void Start()
     {   
+        _index = 0;
         DontDestroyOnLoad(this.gameObject);
     }
+
 
     void Update()
     {
         if (GameManager.Instance == null) return;
-
 
         if (Input.GetKeyDown(KeyCode.H))
         {
@@ -24,9 +26,13 @@ public class DebugManager : MonoBehaviour
         }
         if (!GameManager.Instance.DebugMode) return;
 
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            _index = (_index + 1) % _weaponsToSpawn.Length;
+        }
         if (Input.GetKeyDown(KeyCode.P))
         {
-            ObjectSpawner.Instance.SpawnObjectRpc(_weaponToSpawn, GameManager.Instance.Player1.transform.position, Quaternion.identity);
+            ObjectSpawner.Instance.SpawnObjectRpc(_weaponsToSpawn[_index], GameManager.Instance.Player1.transform.position, Quaternion.identity);
         }
     }
 }
