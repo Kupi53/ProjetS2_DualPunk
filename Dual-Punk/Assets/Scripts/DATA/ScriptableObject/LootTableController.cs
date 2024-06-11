2a66d7f6b9547b053a688b7d09f744eb5e009be2
Corentin <corentin.louis74@gmail.com>
2024-06-11 12:39:05 +0200

    Revert "Merge branch 'zizi'"
    This reverts commit 9aad1b1d123e2662e71bbcbf4aec7dd6bc736926, reversing
    changes made to 2797cabd339492311d3a5c6d5eb26fdd586ea364.

--- Dual-Punk/Assets/Scripts/DATA/ScriptableObject/LootTableController.cs
+++ Dual-Punk/Assets/Scripts/DATA/ScriptableObject/LootTableController.cs
@@ -16,15 +16,7 @@ public class LootTableController : MonoBehaviour
         for (int i =0; i<lootRolls; i++)
         {
             string idToSpawn = lootTable.PickLoot();
-            Vector3 offset = PickOffset();
-            ObjectSpawner.Instance.SpawnObjectFromIdRpc(idToSpawn, gameObject.transform.position + offset, new quaternion());
+            ObjectSpawner.Instance.SpawnObjectFromIdRpc(idToSpawn, gameObject.transform.position, new quaternion());
         }
-    }
-
-    public static Vector3 PickOffset()
-    {
-        int i = UnityEngine.Random.Range(-1, 1);
-        int j = UnityEngine.Random.Range(-1, 1);
-        return new Vector3(i, j, 0);
     }
 }
