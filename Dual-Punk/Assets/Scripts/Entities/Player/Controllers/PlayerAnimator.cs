b5641b00ce59abee2dc93e36e4524eceec0d2862
Corentin <corentin.louis74@gmail.com>
2024-06-14 13:43:31 +0200

    Anim mort joueur

--- Dual-Punk/Assets/Scripts/Entities/Player/Controllers/PlayerAnimator.cs
+++ Dual-Punk/Assets/Scripts/Entities/Player/Controllers/PlayerAnimator.cs
@@ -46,6 +46,7 @@ public class PlayerAnimator : NetworkBehaviour
         if (!IsOwner) return;
         if (_playerState.IsDown)
         {
+            StartCoroutine(DeadAnimation());
             gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.6f);
             ChangeAnimation(PLAYER_DEAD);
         }
@@ -57,6 +58,12 @@ public class PlayerAnimator : NetworkBehaviour
             ChangeAnimation(PLAYER_IDLE);
     }
 
+    private IEnumerator DeadAnimation()
+    {
+        _deadAnimation.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        _deadAnimation.SetActive(true);
+    }
 
     // Utilise dans anim mouvement, change l'animation en fonction des constantes Player_S, Player_N...
     private void ChangeAnimation(string newState)
