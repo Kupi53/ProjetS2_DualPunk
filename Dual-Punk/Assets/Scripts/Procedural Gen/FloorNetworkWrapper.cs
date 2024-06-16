b7f6064afaf61ed5ee4abfc92fba5a283f9e2b7c
mael/splits <mael.minard@epita.fr>
2024-06-16 02:36:12 +0200

    enleve du debug

--- Dual-Punk/Assets/Scripts/Procedural Gen/FloorNetworkWrapper.cs
+++ Dual-Punk/Assets/Scripts/Procedural Gen/FloorNetworkWrapper.cs
@@ -51,12 +51,6 @@ public class FloorNetworkWrapper : NetworkBehaviour
         {
             CheckEnemiesOOB();
         }
-
-        // A SUPR
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            StartEndFight();
-        }
     }
 
     public void NewFloor(FloorType floorType)
